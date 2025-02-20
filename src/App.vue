86eed85fb387708a53e604f56e474edaf3b3b5ae
carlos mendez <181876093+carlosmendez-dev@users.noreply.github.com>
2025-02-19 23:50:00 -0600

    se solocionó el error del boton de VR que no aparecia

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import "aframe";
 </script>
 
 <template>
-    <main>
+    <main class="absolute h-screen w-screen">
         <a-scene>
             <a-box position="0 1 -2" color="#77B6EA"></a-box>
         </a-scene>
