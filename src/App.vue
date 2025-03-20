7a50eef345306c52012c41c2661cf7a2e4096745
carlos mendez <181876093+carlosmendez-dev@users.noreply.github.com>
2025-03-20 08:15:14 -0600

    se actualizó el diseño del boton de menu

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <script setup>
 import "aframe";
-import { onMounted,ref } from "vue";
+import { onMounted,reactive,ref } from "vue";
 const count = ref("");
 const oxigen = ref(100)
+
+
+const btnPosition = reactive({
+    x:0,
+    y:1.5,
+    z:-3
+})
+
 onMounted(()=>{
     // reloj de visor
     setInterval(()=>{
@@ -19,6 +27,7 @@ onMounted(()=>{
         }
     })
 })
+
 </script>
 
 <template>
@@ -30,9 +39,9 @@ onMounted(()=>{
                 <a-cursor material="color:white"></a-cursor>
                 <a-text :value="'Oxygen: ' + oxigen + '%'" scale="0.5 0.5" opacity="0.5" color="#3192d3" position="2 2 -3"></a-text>
                 <a-text :value="'SpO2: ' + oxigen + '%'" scale="0.4 0.4" opacity="1" color="#3192d3" position="-0.5 -1.75 -3"></a-text>
-                <a-image src="/proyecto-vr/visor.png" alpha-test position="0 0 -0.6" width="2"></a-image>
+                <a-image ref="btn" src="/proyecto-vr/visor.png" alpha-test position="0 0 -0.6" width="2"></a-image>
             </a-camera>
-            <a-image width="2.5" src="/proyecto-vr/button.png" position="0 1.5 -3"></a-image>
+            <a-image width="2.5" src="/proyecto-vr/button.png" :position="btnPosition.x + ' ' + btnPosition.y + ' ' + btnPosition.z"></a-image>
                 <a-text :value="count" scale="0.3 0.3" opacity="0.5" color="#3192d3" position="-0.9 2 -3"></a-text>
                 <a-sky color="#040D19"></a-sky>
         </a-scene>
