51134df8bb51638f10a80329fba1ea6ec96fc266
carlos mendez <181876093+carlosmendez-dev@users.noreply.github.com>
2025-04-09 10:59:21 -0600

    test de funcionamiento

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,37 @@
 <script setup>
 import "aframe";
+import "aframe-extras"
+import { computed } from 'vue'
 import { onMounted,reactive,ref } from "vue";
+import { useGamepad } from '@vueuse/core'
 const count = ref("");
 const oxigen = ref(100)
 
 
-const btnPosition = reactive({
-    x:0,
-    y:1.5,
-    z:-3
+const spiderPosition = reactive({
+    x:-5,
+    y:0,
+    z:-3  
 })
 
-const btnDangerPosition = reactive({
-    x:0,
-    y:0.5,
-    z:-3
+
+
+
+onMounted(()=>{
+    setInterval(()=>{
+        spiderPosition.z += 0.5
+    },100)
 })
+
+const btnPosition = reactive({
+    x:-10,
+    y:-5,
+    z:-20
+})
+
+function handleClick(){
+    alert("clicked!")
+}
 
 onMounted(()=>{
     // reloj de visor
@@ -38,17 +54,24 @@ onMounted(()=>{
 
 <template>
     <main class="absolute h-full w-full">
-        <a-scene >
+        <a-scene xr-mode-ui="enabled: true">
             <a-assets>
             </a-assets>
-            <a-camera>
+            <a-camera gamepad-controls="enabled:true">
                 <a-cursor material="color:trasparent"></a-cursor>
-                <a-text :value="'Oxygen: ' + oxigen + '%'" scale="0.4 0.4" opacity="0.7" color="white" position="-1.1 1.50 -3"></a-text>
+                <a-text :value="gamepad" scale="0.4 0.4" opacity="0.7" color="white" position="-1.1 1.50 -3"></a-text>
                 <a-image ref="btn" src="/proyecto-vr/visor.png" alpha-test position="0 0 -0.6" width="2"></a-image>
+                <a-gltf-model  src="/proyecto-vr/rifle/scene.gltf" position="5 -4 -10" rotation=" 0 90 0" scale="0.1 0.1 0.1"></a-gltf-model>
             </a-camera>
+            <a-gltf-model  src="/proyecto-vr/forest/scene.gltf" position="0 -5 1" rotation=" 0 -10 0" scale="2 2 2"></a-gltf-model>
+            <a-gltf-model animation-mixer @click="handleClick"  src="/proyecto-vr/spider/scene.gltf" :position="' ' + spiderPosition.x + ' ' + spiderPosition.y + ' ' + spiderPosition.z" rotation="0 0 0" scale="0.3 0.3 0.3"></a-gltf-model>
+            <button></button>
             <a-image width="2.5" src="/proyecto-vr/button.png" :position="btnPosition.x + ' ' + btnPosition.y + ' ' + btnPosition.z"></a-image>
                 <a-text :value="count" scale="0.3 0.3" opacity="0.5" color="#3192d3" position="-0.9 2 -3"></a-text>
                 <a-sky color="#040D19"></a-sky>
+                <a-entity oculus-go-controls></a-entity>
+                <a-entity oculus-touch-controls="hand: left"></a-entity>
+                <a-entity oculus-touch-controls="hand: right"></a-entity>
         </a-scene>
     </main>
 </template>
